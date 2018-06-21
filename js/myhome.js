c66e514ca69439f064b1c721c5ab304007c0e057
MLrsign <853138399@qq.com>
2018-06-21 14:39:13 +0800

    add picture hover animate and dotted click animate

--- js/myhome.js
+++ js/myhome.js
@@ -4,7 +4,7 @@ $(function () {
 
     //导航栏下拉
     function navDown() {
-        $(".nav-content").stop().slideDown(1000);
+        $(".nav-content").stop().slideDown(500);
         var index = $(this).index();
         var navLi = $(".nav-content-list>li")
         navLi.eq(index).siblings().removeClass("nav-show")
@@ -12,7 +12,7 @@ $(function () {
     }
     //导航栏收起
     function navUp() {
-        $(".nav-content").stop().slideUp(1000);
+        $(".nav-content").stop().slideUp(500);
     }
     //导航栏效果
     $(".nav-list li").on("mouseenter",navDown);
@@ -30,10 +30,11 @@ $(function () {
 
     //轮播图
     var picUl = $(".pic");
+    var dotted = $(".dotted>li");
+    var timer;
     changeDotted(0);
+    autoPlay();
     function changeDotted(index) {
-        console.log(index)
-        var dotted = $(".dotted>li");
         dotted.eq(index).siblings().animate({
             width : "20px"
         },250);
@@ -51,9 +52,35 @@ $(function () {
             case -2400 : picUl.css("left",0);changeDotted(0);break;
         }
     }
-    var timer = setInterval(function () {
-        picUl.animate({
-            left : "-=600px"
-        },500,changePic)
-    },2000)
+    function autoPlay() {
+        timer = setInterval(function () {
+            picUl.animate({
+                left : "-=600px"
+            },500,changePic)
+        },2000)
+    }
+    //轮播图鼠标移入以及点击切换
+    var picLi = $(".pic>li");
+
+    function stopPic() {
+        console.log($(this));
+        clearInterval(timer);
+    }
+    function startPic() {
+        console.log($(this));
+        autoPlay();
+    }
+    function clickDotted() {
+        var index = $(this).index();
+        var newLeft = index * -600;
+        changeDotted(index);
+
+        picUl.stop().animate({
+            left : newLeft + "px"
+        },1000);
+
+    }
+    dotted.hover(stopPic,startPic);
+    picLi.hover(stopPic,startPic);
+    dotted.on("click",clickDotted);
 })
